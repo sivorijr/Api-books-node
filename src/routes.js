047e2d3e5ac0458d70831373f12a3afbb6cdf1ae
Sivori <sivori_jr@hotmail.com>
2020-05-08 21:53:07 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ const routes = express.Router();
 
 const BookController = require("../controllers/BookController");
 
+routes.get("/", (req, res) => { return res.send("BOOKS API by Sivori Junior") });
+
 routes.get("/books", BookController.getAll);
 
 routes.post("/book", BookController.set);
